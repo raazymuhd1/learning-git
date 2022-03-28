0df43a9cc5aa1ab3b6341d178b89e798f820f37f
mohammed raazy <muhammadrazi51@gmail.com>
2022-03-28 21:56:29 +0700

    creating a new element

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-const person = {
-    name: "mohammed",
-    age: 20
+class makeChild {
+    constructor(tagName, text) {
+        this.tagName = tagName;
+        this.text = text;
+    }
+
+    createNewTag() {
+        const container = document.getElementsByClassName("container")[0];
+        const newTag = document.createElement("h2");
+        const newText = document.createTextNode("new Element");
+
+        newTag.appendChild(newText);
+        container.appendChild(newTag)
+    }
+
+    replaceChild() {
+        // const firstChild = this.createNewTag();
+
+        console.log(this.createNewTag())
+    }
 }
 
-const { name, age } = person;
+const getTag = new makeChild();
+// getTag.createNewTag()
+getTag.replaceChild()
